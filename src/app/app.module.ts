2c08e41cb2f59feab63ed5002e21d9843a1a2ba9
Hani <ummehanimohammed9@gmail.com>
2023-03-28 16:54:45 +0530

    Routing
    - The RouterOutlet Placeholde
    - Creating More Routes
    - The RouterLink Directive
    - Active Link Styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AboutModule } from './about/about.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactModule } from './contact/contact.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    ContactModule,
+    AboutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
