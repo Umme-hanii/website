fbac583750a703563130362c9729819515024083
Hani <ummehanimohammed9@gmail.com>
2023-03-30 02:17:49 +0530

    Emitting Events

--- src/app/contact/modal/modal.component.ts
+++ src/app/contact/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, OnDestroy } from '@angular/core';
+import { Component, OnInit, ElementRef, OnDestroy, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -6,6 +6,8 @@ import { Component, OnInit, ElementRef, OnDestroy } from '@angular/core';
   styleUrls: ['./modal.component.css']
 })
 export class ModalComponent implements OnInit, OnDestroy{
+
+  @Output() close = new EventEmitter()
 
   constructor(private el: ElementRef) { }
 
@@ -15,5 +17,9 @@ export class ModalComponent implements OnInit, OnDestroy{
   
   ngOnDestroy(): void {
     this.el.nativeElement.remove()
+  }
+
+  closeModal(): void {
+    this.close.emit()
   }
 }
